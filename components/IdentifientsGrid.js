380f5e0bdfedc67eda2f3a06dbaac1b4c117e71f
MargauxEmma <margaux.barbarin@2024.icam.fr>
2023-04-19 15:07:47 +0200

    camera

--- components/IdentifientsGrid.js
+++ components/IdentifientsGrid.js
@@ -21,7 +21,7 @@ function IdentifientsGrid({profil,title,imageUrl,quiz}){
                             setModalVisible(!modalVisible);
                             }}>
                             <View style={styles.modalView}>
-                                <Text style={styles.modalText}>Le quiz ici</Text>
+                                <Text style={styles.modalText}>{quiz}</Text>
                                 <Pressable
                                 style={[styles.button, styles.buttonClose]}
                                 onPress={() => setModalVisible(!modalVisible)}>
--- data/id.js
+++ data/id.js
@@ -1,8 +1,8 @@
 import identifiants from "../models/identifiants";
 
 export const IDENTIFIANTS =[
-    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c1','@FredIcam','https://www.m-habitat.fr/medias/image/faire-appel-a-un-jardinier-pour-entretenir-son-jardin-1933-640-0.jpg','QUIZ'),
-    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c2', '@Clara', 'https://www.denormandie.fr/wp-content/uploads/2020/11/plantesvertes.jpg','QUIZ'),
-    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c3', '@Alix','https://bergamotte.imgix.net/jsg26asrsg3w6by10a6xdu2p2pv9?ixlib=rails-4.2.0&auto=format%2Ccompress&fit=crop&q=65&ar=528%3A660','QUIZ'),
-    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c4', '@Margaux','https://static.actu.fr/uploads/2016/07/jardinage-femme_bellastudio-fotolia.jpg','QUIZ'),
+    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c1','@FredIcam','https://www.m-habitat.fr/medias/image/faire-appel-a-un-jardinier-pour-entretenir-son-jardin-1933-640-0.jpg','Que represente cette image? \n \n Arrose des plante  \n Tri ses déchets'),
+    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c2', '@Clara', 'https://www.denormandie.fr/wp-content/uploads/2020/11/plantesvertes.jpg','Que represente cette image? \n \n Arrose des plante  \n Tri ses déchets'),
+    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c3', '@Alix','https://bergamotte.imgix.net/jsg26asrsg3w6by10a6xdu2p2pv9?ixlib=rails-4.2.0&auto=format%2Ccompress&fit=crop&q=65&ar=528%3A660','Que represente cette image? \n \n Arrose des plante  \n Tri ses déchets'),
+    new identifiants('https://meetanentrepreneur.lu/wp-content/uploads/2019/08/profil-linkedin-1.jpg','c4', '@Margaux','https://static.actu.fr/uploads/2016/07/jardinage-femme_bellastudio-fotolia.jpg','Que represente cette image? \n \n Arrose des plante  \n Tri ses déchets'),
 ];
